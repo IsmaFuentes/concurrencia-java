a7adfa101882404ba9ab4b9bab527dde6016ae57
Ismael Fuentes <ismafuentes@hotmail.es>
2022-11-15 16:50:04 +0100

    Solución barberos

---
+++ EjercicioConcurrencia/src/barberos/solution/Cliente.java
@@ -0,0 +1,17 @@
+package barberos.solution;
+
+public class Cliente  {
+  GestorSillas gestorSillas;
+  public Cliente(GestorSillas g){
+    this.gestorSillas = g;
+  }
+  public void entrarEnBarberia(){
+    int posSillaLibre = this.gestorSillas.getPosSillaLibre();
+    if (posSillaLibre == -1){
+      System.out.println("No habia sillas libres, me marcho");
+      return;
+    }
+
+    System.out.println("Me siento en la silla:"+posSillaLibre);
+  }
+}
